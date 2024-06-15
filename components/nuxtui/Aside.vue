0f30e71dd3633ff117638c9758f1be3555715e1a
andijayawizard <andijayawizard@gmail.com>
2024-06-15 15:54:51 +0700

    15jun2024

---
+++ components/nuxtui/Aside.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+const route = useRoute()
+const links = [{
+  label: 'Profile',
+  avatar: {
+    src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+  },
+  badge: 100
+}, {
+  label: 'Home',
+  icon: 'i-heroicons-home',
+  to: '/'
+}, {
+  label: 'Menu Website',
+  icon: '',
+  to: '/menu-website'
+}, {
+  label: 'Blog',
+  icon: "",
+  to: '/blog'
+}, {
+  label: 'Services',
+  icon: 'i-heroicons-command-line',
+  to: '/services'
+}, {
+  label: 'Social Media',
+  icon: '',
+  to: '/social-media'
+}, {
+  label: 'Users',
+  icon: 'i-heroicons-chart-bar',
+  to: '/users'
+  // to: `${route.path.startsWith('/dev') ? '/dev' : ''}/components/horizontal-navigation`
+}, {
+  label: 'Image Slides',
+  icon: '',
+  to: '/image-slide'
+}, {
+  label: 'Cases Studies',
+  icon: '',
+  to: '/cases-studies'
+}, {
+  label: 'Brands',
+  icon: 'i-heroicons-light-bulb',
+  to: '/brands'
+}, {
+  label: 'Products',
+  icon: 'i-heroicons-question-mark-circle',
+  to: '/products'
+}]
+
+const pageLinks = [{
+  icon: 'i-heroicons-heart',
+  label: 'Learn how to contribute',
+  to: '/getting-started/contributing',
+  target: '_blank'
+}]
+</script>
+
+<template>
+  <UVerticalNavigation :links="links" />
+</template>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,42 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+</script>
 
 <template>
+  <UContainer>
+    <UPage>
+      <div class="flex flex-wrap">
+        <div class="w-full sm:w-1/3 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-black-500--">
+          <div class="mr-4">
+            <NuxtuiAside />
+          </div>
+        </div>
+        <div class="w-full sm:w-2/3 md:w-2/3 lg:w-3/4 xl:w-5/6 mb-4 bg-gray-400--">
+          <div class="ml-4">
+            <Breadcrumb />
+            <slot />
+          </div>
+        </div>
+      </div>
+      <!-- <Navbar /> -->
+    </UPage>
+  </UContainer>
 
-  <body
+  <!-- <body
     class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed text-sm control-sidebar-slide-open">
     <div class="wrapper">
       <Navbar />
       <Breadcrumb />
-      <!-- <Aside /> -->
+      <Aside />
       <div class="content-wrapper">
         <Header />
         <section class="content">
           <div id="load"></div>
           <slot />
-          <!--/. container-fluid -->
         </section>
       </div>
       <Footer />
     </div>
-  </body>
+  </body> -->
 </template>
 
 <style scoped></style>
